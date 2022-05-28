9222955a54296dad721046287c351a877a75dd4d
sa5i <106051204+sa5i@users.noreply.github.com>
2022-05-28 22:11:29 +0530

    working add function

--- test.java
+++ test.java
@@ -2,10 +2,10 @@ class Test{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
      System.out.println("Hai SASI Welcome to Github");
-     public add{
+     
          int a =10;
          int b= 9;
          int c = a+b;
-         System.out.println(c);
+         System.out.println("Addition of a and b is:"+c);
     }  
 }  
