d1a02ebab75d88c4d9df9d32ba807fc1e5fe5ebd
sa5i <106051204+sa5i@users.noreply.github.com>
2022-05-28 22:10:41 +0530

    add funtion

--- test.java
+++ test.java
@@ -2,5 +2,10 @@ class Test{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
      System.out.println("Hai SASI Welcome to Github");
+     public add{
+         int a =10;
+         int b= 9;
+         int c = a+b;
+         System.out.println(c);
     }  
 }  
